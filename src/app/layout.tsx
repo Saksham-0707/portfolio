b7332311e0bbf6a29d36cf52414533166556e912
Saksham Doda <sakshamd23@nitj.ac.in>
2025-04-03 13:53:12 +0530

    initial without projects and experience

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from 'next'
+import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
+import { NavigationBar } from '@/components/nav-bar'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { GeistSans } from "geist/font/sans";
+
+
+export const metadata: Metadata = {
+    title: 'Saksham Doda',
+    description: 'I plan, design, build, test and deploy software',
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="en">
+            <body className={GeistSans.className}>
+                <ThemeProvider
+                    attribute='class'
+                    defaultTheme='dark'
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <NavigationBar />
+                    <main className='container mx-auto'>
+                        {children}
+                    </main>
+                    <footer className="container mx-auto py-4">
+                        <p className="text-center text-sm text-muted-foreground hover:text-white transition-all">
+                            © Designed & Built by Saksham Doda· 2025
+                        </p>
+                    </footer>
+                </ThemeProvider>
+                <Analytics />
+                <SpeedInsights />
+            </body>
+        </html>
+    )
+}
